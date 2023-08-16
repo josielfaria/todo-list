30cdebfb8b31f92b390edc1dce3f581a7c024ad5
Josiel Faria <josielsifa@gmail.com>
2023-08-16 20:17:18 -0300

    feat: config routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationPageComponent } from './page/registration-page/registration-page.component';
+import { LoginPageComponent } from './page/login-page/login-page.component';
+import { HomePageComponent } from './page/home-page/home-page.component';
+import { TodoListPageComponent } from './page/todo-list-page/todo-list-page.component';
+import { NewTaskPageComponent } from './page/new-task-page/new-task-page.component';
+import { EditTaskPageComponent } from './page/edit-task-page/edit-task-page.component';
+import { RemoveTaskPageComponent } from './page/remove-task-page/remove-task-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationPageComponent,
+    LoginPageComponent,
+    HomePageComponent,
+    TodoListPageComponent,
+    NewTaskPageComponent,
+    EditTaskPageComponent,
+    RemoveTaskPageComponent
   ],
   imports: [
     BrowserModule,
