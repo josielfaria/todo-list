84a12e53b08f1237d126f8bb04b4a60591e113da
Josiel Faria <josielsifa@gmail.com>
2023-08-16 20:17:05 -0300

    feat: create page components

---
+++ src/app/page/remove-task-page/remove-task-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-task-page',
+  templateUrl: './remove-task-page.component.html',
+  styleUrls: ['./remove-task-page.component.scss']
+})
+export class RemoveTaskPageComponent {
+
+}
