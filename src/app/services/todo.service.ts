b533f37e38eb69e7a6a23395b76d9bdade5e124c
Josiel Faria <josielsifa@gmail.com>
2023-08-17 13:47:27 -0300

    feat: add get todo and update todo

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { TodoModel } from '../models/todo.model';
@@ -12,11 +12,50 @@ export class TodoService {
   constructor(private httpClient: HttpClient) {}
 
   getAllTodos(): Observable<Array<TodoModel>> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     return this.httpClient
-      .get<Array<TodoModel>>(this.URL_API + '/todo-list')
+      .get<Array<TodoModel>>(this.URL_API + '/todo-list', { headers })
       .pipe(
         tap((response: Array<TodoModel>) => {
-          console.log('response', response)
+          // TODO: rever esse retorno
+          console.log('response', response);
+          if (response && response.length > 0) {
+          } else {
+          }
+        })
+      );
+  }
+
+  getTodo(id: string): Observable<Array<TodoModel>> {
+    const params = new HttpParams().append('id', id);
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.httpClient
+      .get<Array<TodoModel>>(this.URL_API + '/todo-list', { params, headers })
+      .pipe(
+        tap((response: Array<TodoModel>) => {
+          console.log('response', response);
+          // TODO: rever esse retorno
+          if (response && response.length > 0) {
+          } else {
+          }
+        })
+      );
+  }
+
+  updateTodo(newTodo: TodoModel): Observable<Array<TodoModel>> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.httpClient
+      .put<Array<TodoModel>>(
+        this.URL_API + `/todo-list/${newTodo.id}`,
+        newTodo,
+        { headers }
+      )
+      .pipe(
+        tap((response: Array<TodoModel>) => {
+          console.log('response update', response);
+          // TODO: rever esse retorno
           if (response && response.length > 0) {
           } else {
           }
