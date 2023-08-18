00a05fc7095bf6e84ce880e54433814983ae9838
Josiel Faria <josielsifa@gmail.com>
2023-08-18 14:46:22 -0300

    feat: create page for resgistration new user

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, catchError, map, of } from 'rxjs';
+import { UserModel } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss']
+  styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage {
+export class SignupPage implements OnInit {
+  registrationForm: FormGroup | undefined;
 
+  usernameExists = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.setRegistrationFormGroupConfig();
+  }
+
+  registration() {
+    this.registrationForm?.markAllAsTouched();
+    if (this.registrationForm?.valid) {
+      const newUser = this.registrationForm.getRawValue() as UserModel;
+      if (newUser) {
+        this.authService.register(newUser).subscribe((userRegistered) => {
+          console.log('userRegistered', userRegistered);
+          if (userRegistered) {
+            this.router.navigateByUrl('/signin');
+          } else {
+            this.registrationForm
+              ?.get('username')
+              ?.setErrors({ usernameExists: true });
+          }
+        });
+      }
+    }
+  }
+
+  private setRegistrationFormGroupConfig(): void {
+    this.registrationForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      username: [
+        '',
+        [Validators.required, Validators.minLength(4)],
+        [this.usernameExistsValidator()],
+      ],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      confirmPassword: [
+        '',
+        [Validators.required],
+        [this.checkPasswordsValidator()],
+      ],
+    });
+  }
+
+  private usernameExistsValidator(): AsyncValidatorFn {
+    return (
+      control: AbstractControl
+    ): Observable<{ [key: string]: any } | null> => {
+      const username = control.value;
+      return this.authService.checkUsernameExists(username).pipe(
+        map((exists) => (exists ? { usernameExists: true } : null)),
+        catchError(() => of(null))
+      );
+    };
+  }
+
+  private checkPasswordsValidator(): AsyncValidatorFn {
+    return (
+      control: AbstractControl
+    ): Observable<{ [key: string]: any } | null> => {
+      const password = this.registrationForm?.get('password')?.value;
+      const confirmPassword = control?.value;
+      const passwordsMatch = password !== confirmPassword;
+      return of(passwordsMatch ? { passwordsDoNotMatch: true } : null);
+    };
+  }
 }
