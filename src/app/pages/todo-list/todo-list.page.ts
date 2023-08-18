9db7ae4777e72b97c743495692d18128618d3ed9
Josiel Faria <josielsifa@gmail.com>
2023-08-18 00:08:08 -0300

    feat: use mock with rxjs

--- src/app/pages/todo-list/todo-list.page.ts
+++ src/app/pages/todo-list/todo-list.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TodoModel } from 'src/app/models/todo.model';
-import { TodoService } from 'src/app/services/todo.service';
 import { ActionTodoPageEnum } from 'src/app/enums/action-todo-page';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -18,13 +18,16 @@ export class TodoListPage implements OnInit {
   constructor(private router: Router, private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.todoService.getAllTodos().subscribe((todoList) => {
+    this.todoService.getTodoList().subscribe((todoList) => {
       this.todoList = todoList;
     });
   }
 
   navigateTo(action: ActionTodoPageEnum, idTodo: string): void {
-    let url = action === ActionTodoPageEnum.EditTodoPage ? '/edit-todo' : '/remove-todo';
+    let url =
+      action === ActionTodoPageEnum.EditTodoPage
+        ? '/edit-todo'
+        : '/remove-todo';
     this.router.navigate([url], { queryParams: { idTodo, action } });
   }
 
