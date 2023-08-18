087a488fca13d119c512160a31774a71f74d42a8
Josiel Faria <josielsifa@gmail.com>
2023-08-18 16:16:13 -0300

    feat: create sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit, OnDestroy {
+  private subscriptions = new Subscription();
+
+  isLoggedIn = this.authService.isLoggedIn();
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.authService.loggedIn$.subscribe((isLoggedIn) => {
+        this.isLoggedIn = isLoggedIn;
+        if (!isLoggedIn && this.authService.isLoggedIn()) {
+          this.router.navigateByUrl('signin');
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
