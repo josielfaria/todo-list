40512cf7ff24602a01c3463f10649bd1fa5dd2f0
Josiel Faria <josielsifa@gmail.com>
2023-08-19 13:46:38 -0300

    feat: add test loading component

--- src/app/components/loading/loading.component.spec.ts
+++ src/app/components/loading/loading.component.spec.ts
@@ -1,21 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoadingComponent } from './loading.component';
+import { LoadingService } from 'src/app/services/loading.service';
+import { of } from 'rxjs';
 
 describe('LoadingComponent', () => {
   let component: LoadingComponent;
   let fixture: ComponentFixture<LoadingComponent>;
+  let mockLoadingService: Partial<LoadingService>;
 
   beforeEach(() => {
+    mockLoadingService = {
+      loading$: of(true),
+    };
+
     TestBed.configureTestingModule({
-      declarations: [LoadingComponent]
+      declarations: [LoadingComponent],
+      providers: [{ provide: LoadingService, useValue: mockLoadingService }],
     });
+
     fixture = TestBed.createComponent(LoadingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve ser criado o component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve definir isLoading como true quando o loadingService emite true', () => {
+    mockLoadingService.loading$ = of(true);
+    fixture.detectChanges();
+    expect(component.isLoading).toBeTrue();
+  });
+
+  it('deve definir isLoading como false quando o loadingService emite false', () => {
+    mockLoadingService.loading$ = of(false);
+    fixture.detectChanges();
+    expect(component.isLoading).toBeFalse();
+  });
+
+  it('deve cancelar a inscrição no loadingService em ngOnDestroy', () => {
+    const unsubscribeSpy = spyOn(component['subscription'], 'unsubscribe');
+    mockLoadingService.loading$ = of(true);
+    fixture.detectChanges();
+    component.ngOnDestroy();
+    expect(unsubscribeSpy).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 });
